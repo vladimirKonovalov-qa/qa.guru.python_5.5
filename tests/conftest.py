089b05c68e6d485f7f8782f1de9031ed4983ea59
vladimirKonovalov-qa <vkonovalov74@gmail.com>
2023-04-24 22:14:51 +0500

    Second commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selene import browser
-import os
+
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_action():
-    browser.open('https://demoqa.com/automation-practice-form')
+    browser.config.base_url = 'https://demoqa.com'
--- tests/homework.py
+++ tests/homework.py
@@ -1,23 +1,41 @@
 from selene import browser
-from selene import be, have
+from selene import have, command
 import os
 
 
 def test_registration_form():
+    browser.open('/automation-practice-form')
     browser.element('#firstName').type('Ivan')
     browser.element('#lastName').type('Ivanov')
     browser.element('#userEmail').type('IvanovIvan@mail.ru')
     browser.element('#gender-radio-1').double_click()
-    browser.element('#userNumber').type('+990989968')
+    browser.element('#userNumber').type('1990989968')
     browser.element('#dateOfBirthInput').click()
     browser.element('[class="react-datepicker__month-select"]').click().element('[value="6"]').click()
     browser.element('[class="react-datepicker__year-select"]').click().element('[value="1990"]').click()
     browser.element('[aria-label="Choose Tuesday, July 3rd, 1990"]').click()
     browser.element('#subjectsInput').type('English').press_enter().type('Commerce').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('#uploadPicture').send_keys(os.getcwd()+'tests/resources/agggg.jpg')
-
+    browser.element('#uploadPicture').send_keys(os.getcwd()+'/resources/agggg.jpg')
     browser.element('#currentAddress').type('163 McGlynn Crossroad North Madonnachester, VIC 2092')
+    browser.element('#react-select-3-input').type('NCR').press_enter()
+    browser.element('#react-select-4-input').type('Delhi').press_enter()
+    browser.element('#submit').perform(command.js.scroll_into_view)
+    browser.element('#submit').press_enter()
+
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.all('.table td:nth-child(2)').should(have.exact_texts(
+        'Ivan Ivanov',
+        'IvanovIvan@mail.ru',
+        'Male',
+        '1990989968',
+        '03 July,1990',
+        'English, Commerce',
+        'Sports',
+        'agggg.jpg',
+        '163 McGlynn Crossroad North Madonnachester, VIC 2092',
+        'NCR Delhi'))
+
 
 
 
